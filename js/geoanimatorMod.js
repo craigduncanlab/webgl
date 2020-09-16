a628b1362a5ff1367623c05b157e652ed38f4168
Craig Duncan <software@craigduncan.com.au>
2020-09-16 15:07:21 +0800

    Modify general functions

--- js/geoanimatorMod.js
+++ js/geoanimatorMod.js
@@ -14,9 +14,9 @@ Here, the code is written on the assumption that you are running it over a serve
 three.js library is available in the three.js-master folder (downloaded as zip).
 When using as modules, the /jsm rather than /js folder should be used */
 
-import * as THREE from "../three.js-master/build/three.module.js"; //relative to this file
-import {OrbitControls} from "../three.js-master/examples/jsm/controls/OrbitControls.js"; //choose the jsm folder for module bindings
-import {GLTFLoader} from "../three.js-master/examples/jsm/loaders/GLTFLoader.js";
+import * as THREE from "../three-js.master/build/three.module.js"; //relative to this file
+import {OrbitControls} from "../three-js.master/examples/jsm/controls/OrbitControls.js"; //choose the jsm folder for module bindings
+import {GLTFLoader} from "../three-js.master/examples/jsm/loaders/GLTFLoader.js";
 
 //the THREE prefix will only apply to the methods in the three.module.js file.
 
@@ -53,6 +53,7 @@ function init() {
 	//--- (a) GEOMETRY ---
 	/*
 	nb see: https://threejs.org/docs/index.html#api/en/core/Geometry
+	vertices are more directly accessible in Geometry than BufferGeometry (slight performance drop)
 	what follows are individual ports of 'BufferGeometry' options
 	nb: "Any modification after instantiation does not change the geometry."
 
@@ -64,15 +65,172 @@ function init() {
 	*/
 
 	//general variables
-	var radius,segments,thetaStart,thetaLength,widthSegments,heightSegments,phiStart,tube, radialSegments,tubularSegments,arc
 	var Pi = 3.141592653;
 
 	
 	
-	//hand code some options
-	var choice=4; //torus
-	var materialtype=1; //mesh options for geometric shapes
+	
+	// --- (b) MATERIAL FOR MESH ---
+	
+	//Different ways to store colour information
 
+	var col1='#8AC'; //blue/gray
+	var col2=0x00ff00  //luminescent green!
+	var col3 = new THREE.Color('pink');
+
+	
+	geometry=getGeometry(4); //torus
+	material=getMaterial(4); //seagull wrap	5=trump
+	//always create objects outside animation loop
+	myObject = new THREE.Mesh( geometry, material );  
+	myObject.position.set(0,100,0);  //This will 'float' above the xz plane
+	// -- Add the completed object to the scene ---
+
+	scene.add( myObject );
+
+	// --- MESH (SECOND OBJECT FOR A PLANE SURFACE TO USE AS GROUND) --- 
+
+	/*
+
+	width — Width along the X axis. Default is 1.
+	height — Height along the Y axis. Default is 1.
+	widthSegments — Optional. Default is 1. 
+	heightSegments — Optional. Default is 1.
+
+	*/
+	var groundGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);  //this avoids need to set 3D vertices directly
+
+	const planeSize = 40; //this is actually number of images that should fill the plane...origin to edge?
+    const gloader = new THREE.TextureLoader();
+    var image_floor1='../TrumpDemo.png'
+    var image_floor2='../Seagull.png'
+    groundTexture=gloader.load(image_floor1); //insert an image as the 'texture'
+    groundTexture.wrapS = THREE.RepeatWrapping;
+    groundTexture.wrapT = THREE.RepeatWrapping;
+    groundTexture.magFilter = THREE.NearestFilter; //
+    var grepeats = planeSize / 2;  //effectively (20,20)
+    groundTexture.repeat.set(grepeats, grepeats);
+    /*
+    // If texture is used for color information, set colorspace.
+	groundTexture.encoding = THREE.sRGBEncoding;
+	// UVs use the convention that (0, 0) corresponds to the upper left corner of a texture.
+	groundTexture.flipY = false;
+	*/
+
+	var groundMaterial = new THREE.MeshBasicMaterial( { map: groundTexture, side: THREE.DoubleSide } ); //up and down
+	
+    var ground = new THREE.Mesh(groundGeometry, groundMaterial);
+    //origin is 0,0
+	ground.position.y = 0;  //y=-50.5
+	ground.rotation.x = Math.PI/2;//0 is default xy plane but this rotation takes it to yz plane so changing z position navigates horizontally to origin
+	
+	scene.add(ground);
+
+	gltfloader = new GLTFLoader();
+	//relative location 
+	var truckfile='../three-js.master/examples/models/gltf/CesiumMilkTruck/glTF/CesiumMilkTruck.gltf'; //truck is in three.js assets
+	var scale1=4.0; //magnify truck
+	var helmetfile='../three-js.master/examples/models/gltf/DamagedHelmet/glTF/DamagedHelmet.gltf';
+	var scale3=12.0; //magnify truck
+	var loadfile5='';
+	var gingerfile='../three-js.master/examples/models/gltf/ginger/gingerman3.gltf';
+	var scale2=0.2; //gingerbread man needs small scale
+	var myObject6=loadObject(gingerfile,scale2,0,0,0); //ground level.  Loads and adds to scene as node.
+	var myObject5=loadObject(truckfile,scale1,0,100,0);
+	var myObject2=loadObject(helmetfile,scale3,0,50,0);
+
+	//try an update to the helmet texture and update:
+	/*
+	gullmaterial=getMaterial(4); //if you can't access the texture directly...replace the whole mesh...
+	myObject2 = new THREE.Mesh()
+	myObject2.material.needsUpdate = true
+	myObject2.material.map.needsUpdate = true; //'map' refers to the texture in the material object.  See arguments to THREE.Mesh..()
+	*/
+
+	//TO DO : reference to positions outside  
+
+	// ---- CAMERA ---
+	//if you make the z val bigger, then camera is further away (your 'z' position is further from origin.  1 is very close.  50 is far.
+	//Some good tips on this here: https://discoverthreejs.com/tips-and-tricks/
+
+	//camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+	
+	/*
+
+	fov — Camera frustum vertical field of view.
+	aspect — Camera frustum aspect ratio.
+	near — Camera frustum near plane.
+	far — Camera frustum far plane.  Once camera position is > far, it loses 'scene'
+
+*/
+	//---setup your camera according to your conventions for the scene...//
+	var far= 500
+	camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, far);  //(fov, aspect, near, far);
+	var camx=0; //(0,100,100)
+	var camy=100;  // if your plane (floor) is xz plane then y values are height.  You are looking with camera so that z is depth though.
+	var camz=250; //if you have a large plane e.g. 1000 x 1000 then this is necessary to 'see' the image at ground etc.
+    camera.position.set(camx, camy, camz);
+	camera.lookAt(0,0,0); //how to store a cubic
+
+    console.log(camera)
+    controls = new OrbitControls(camera,myCanvas); //no need for THREE prefix here.
+ 
+	/* Point light casts shadows; ambient light does not
+	https://threejs.org/docs/#api/en/lights/AmbientLight
+	*/
+	const ltcol = 0xFFFFFF; //white
+	const intensity = 1; //full
+	var lightchoice=1;
+	var light; //for light 'source'
+	if (lightchoice==1) {
+		light = new THREE.PointLight(ltcol);
+	}
+	else {	
+		light = new THREE.AmbientLight(ltcol, intensity); //no shadows
+	}
+	light.position.set(100,100,100); //this is relative... (100,250,100).  Set to light source at moment.
+	//https://threejs.org/docs/#api/en/math/Color
+	//https://github.com/mrdoob/three.js/blob/master/src/math/Color.js
+	scene.background = new THREE.Color('skyblue');
+	scene.add(light);
+}
+
+	
+
+function getMaterial(materialtype) {
+	var material;
+	if (materialtype==1) {
+		material = new THREE.MeshStandardMaterial( {color: col3 }); //args are a list
+	}
+	else if (materialtype==2) {
+		//material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); //luminescent green!
+		//this material is NOT affected by lights.
+		material = new THREE.MeshBasicMaterial( { color: col1 }); 
+	}
+	else if (materialtype==3) {
+		material = new THREE.MeshNormalMaterial(); //basic faces with different colours 
+	}
+	else if (materialtype==4) {
+		// Create a texture loader so we can load the image file
+		var imgloader = new THREE.TextureLoader();
+		material = new THREE.MeshLambertMaterial({
+ 		 	map: imgloader.load('../Seagull.png')
+		});
+	}
+	else if (materialtype==5) {
+		// Create a texture loader so we can load the image file
+		var imgloader = new THREE.TextureLoader();
+		material = new THREE.MeshLambertMaterial({
+ 		 	map: imgloader.load('../TrumpDemo.png')
+		});
+	}
+	return material;
+}
+
+function getGeometry(choice) {
+	var geometry;
+	var radius,segments,thetaStart,thetaLength,widthSegments,heightSegments,phiStart,tube, radialSegments,tubularSegments,arc
+	
 	if (choice==1) {
 		radius = 3; //default is 1
 		segments = 36; //default is 8 (2 x 4 quaters) but you'll need at least 36 in this app to look like a smooth circle.  
@@ -123,143 +281,17 @@ function init() {
 		tubularSegments=100; //for 
 		arc = Math.Pi;
 		geometry = new THREE.TorusGeometry(radius, tube, radialSegments, tubularSegments, arc);
-	}
-	
-	// --- (b) MATERIAL FOR MESH ---
-	
-	//Different ways to store colour information
 
-	var col1='#8AC'; //blue/gray
-	var col2=0x00ff00  //luminescent green!
-	var col3 = new THREE.Color('pink');
-
-	//select some materials
-	if (materialtype==1) {
-		material = new THREE.MeshStandardMaterial( {color: col3 }); //args are a list
-	}
-	else if (materialtype==2) {
-		//material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); //luminescent green!
-		material = new THREE.MeshBasicMaterial( { color: col1 }); 
-	}
-	else if (materialtype==3) {
-		material = new THREE.MeshNormalMaterial(); //basic faces with different colours 
-	}
-	
-	//always create objects outside animation loop
-	myObject = new THREE.Mesh( geometry, material );  
-	//This will 'float' above the xz plane
-
-	myObject.position.x=0;
-	myObject.position.y=100;
-	myObject.position.z=0;
-	// -- Add the completed object to the scene ---
-
-	scene.add( myObject );
-
-	// --- MESH (SECOND OBJECT FOR A PLANE SURFACE TO USE AS GROUND) --- 
-
-	/*
-
-	width — Width along the X axis. Default is 1.
-	height — Height along the Y axis. Default is 1.
-	widthSegments — Optional. Default is 1. 
-	heightSegments — Optional. Default is 1.
-
-	*/
-	var groundGeometry = new THREE.PlaneGeometry(1000, 1000, 10, 10);  //this avoids need to set 3D vertices directly
-
-	const planeSize = 40; //this is actually number of images that should fill the plane...origin to edge?
-    const gloader = new THREE.TextureLoader();
-    var image_floor1='TrumpDemo.png'
-    var image_floor2='Seagull.png'
-    groundTexture=gloader.load(image_floor1); //insert an image as the 'texture'
-    groundTexture.wrapS = THREE.RepeatWrapping;
-    groundTexture.wrapT = THREE.RepeatWrapping;
-    groundTexture.magFilter = THREE.NearestFilter; //
-    var grepeats = planeSize / 2;  //effectively (20,20)
-    groundTexture.repeat.set(grepeats, grepeats);
-    /*
-    // If texture is used for color information, set colorspace.
-	groundTexture.encoding = THREE.sRGBEncoding;
-	// UVs use the convention that (0, 0) corresponds to the upper left corner of a texture.
-	groundTexture.flipY = false;
-	*/
-
-	var groundMaterial = new THREE.MeshBasicMaterial( { map: groundTexture, side: THREE.DoubleSide } ); //up and down
-	
-    var ground = new THREE.Mesh(groundGeometry, groundMaterial);
-    //origin is 0,0
-	ground.position.y = 0;  //y=-50.5
-	ground.rotation.x = Math.PI/2;//0 is default xy plane but this rotation takes it to yz plane so changing z position navigates horizontally to origin
-	
-	scene.add(ground);
-
-	//use callback function to assign obj
-	gltfloader = new GLTFLoader();  //var loader = new GLTFLoader();  //no need to refer to THREE
-	//relative location 
-	var loadfile1='../three.js-master/examples/models/gltf/CesiumMilkTruck/glTF/CesiumMilkTruck.gltf'; //truck is in three.js assets
-	var scale1=4.0; //magnify truck
-	var loadfile3='../three.js-master/examples/models/gltf/DamagedHelmet/glTF/DamagedHelmet.glTF';
-	var scale3=12.0; //magnify truck
-	//var loadfile2='../gingerman2.glb';
-	var loadfile2='../gingerman3.gltf';
-	var scale2=0.2; //gingerbread man needs small scale
-	var myObject3=loadObject(loadfile2,scale2,0,0,0);
-	var myObject5=loadObject(loadfile1,scale1,0,100,0);
-	var myObject2=loadObject(loadfile3,scale3,0,50,0);
-	
-	//TO DO : reference to positions outside  
-
-	// ---- CAMERA ---
-	//if you make the z val bigger, then camera is further away (your 'z' position is further from origin.  1 is very close.  50 is far.
-	//Some good tips on this here: https://discoverthreejs.com/tips-and-tricks/
-
-	//camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-	
-	/*
-
-	fov — Camera frustum vertical field of view.
-	aspect — Camera frustum aspect ratio.
-	near — Camera frustum near plane.
-	far — Camera frustum far plane.  Once camera position is > far, it loses 'scene'
-
-*/
-	//---setup your camera according to your conventions for the scene...//
-	var far= 500
-	camera = new THREE.PerspectiveCamera(45, 4 / 3, 1, far);  //(fov, aspect, near, far);
-	var camx=0; //(0,100,100)
-	var camy=100;  // if your plane (floor) is xz plane then y values are height.  You are looking with camera so that z is depth though.
-	var camz=250; //if you have a large plane e.g. 1000 x 1000 then this is necessary to 'see' the image at ground etc.
-    camera.position.set(camx, camy, camz);
-	camera.lookAt(0,0,0); //how to store a cubic
-
-    console.log(camera)
-    controls = new OrbitControls(camera,myCanvas); //no need for THREE prefix here.
- 
-	/* Point light casts shadows; ambient light does not
-	https://threejs.org/docs/#api/en/lights/AmbientLight
-	*/
-	const ltcol = 0xFFFFFF; //white
-	const intensity = 1; //full
-	var lightchoice=1;
-	var light; //for light 'source'
-	if (lightchoice==1) {
-		light = new THREE.PointLight(ltcol);
-	}
-	else {	
-		light = new THREE.AmbientLight(ltcol, intensity); //no shadows
-	}
-	light.position.set(100,100,100); //this is relative... (100,250,100).  Set to light source at moment.
-	//https://threejs.org/docs/#api/en/math/Color
-	//https://github.com/mrdoob/three.js/blob/master/src/math/Color.js
-	scene.background = new THREE.Color('skyblue');
-	scene.add(light);
+		//we also set the image for the material here
+		}
+		return geometry;
 }
 
 //async: interior function loader may not have executed before 'loadObject' returns 
 function loadObject( loadfile, scale1,x,y,z ){
+	//use callback function to assign obj  //var loader = new GLTFLoader();  //no need to refer to THREE
 	gltfloader.load(loadfile, function(obj) {
-        var character = obj.scene;
+        var character = obj.scene; //are these just scene elements??
         character.position.set(x,y,z);
         character.rotation.x=0; //Math.PI;  x is 'out' of the image plane yz at moment.  So x rotations put it behind image.
         character.rotation.y=Math.PI; //we've rotated the image so it sits flat on the xz plane...?
